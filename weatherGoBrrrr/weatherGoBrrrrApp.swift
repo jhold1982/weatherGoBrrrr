e629724cdc4755e05c084076dfd7ab158202777d
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2025-02-19 08:18:09 -0600

    Initial Commit

---
+++ weatherGoBrrrr/weatherGoBrrrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherGoBrrrrApp.swift
+//  weatherGoBrrrr
+//
+//  Created by Justin Hold on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherGoBrrrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
